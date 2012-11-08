f635e379812c0033dc77b29e51cadaeea2e5e9ba
David Windell <david@outeredgeuk.com>
2012-11-08 17:13:21 +0000

    updated to match trait

--- src/Edge/EventManager/AbstractEventProvider.php
+++ src/Edge/EventManager/AbstractEventProvider.php
@@ -4,37 +4,52 @@ namespace Edge\EventManager;
 
 use Zend\EventManager\EventManagerAwareInterface;
 use Zend\EventManager\EventManagerInterface;
+use Traversable;
 
-abstract class AbstractEventProvider implements EventManagerAwareInterface {
-
+abstract class AbstractEventProvider implements EventManagerAwareInterface
+{
     /**
      * @var EventManagerInterface
      */
     protected $events;
 
     /**
-     * Set the event manager instance
+     * Set the event manager instance used by this context
      *
-     * @param  EventManagerInterface $eventManager
-     * @return AbstractEventProvider
+     * @param  EventManagerInterface $events
+     * @return mixed
      */
-    public function setEventManager(EventManagerInterface $eventManager) {
-        $eventManager->setIdentifiers(array(
-            __CLASS__,
-            get_called_class(),
-            'Zebreco',
-        ));
-        $this->events = $eventManager;
+    public function setEventManager(EventManagerInterface $events)
+    {
+        $identifiers = array(__CLASS__, get_called_class());
+        if (isset($this->eventIdentifier)) {
+            if ((is_string($this->eventIdentifier))
+                || (is_array($this->eventIdentifier))
+                || ($this->eventIdentifier instanceof Traversable)
+            ) {
+                $identifiers = array_unique(array_merge($identifiers, (array) $this->eventIdentifier));
+            } elseif (is_object($this->eventIdentifier)) {
+                $identifiers[] = $this->eventIdentifier;
+            }
+            // silently ignore invalid eventIdentifier types
+        }
+        $events->setIdentifiers($identifiers);
+        $this->events = $events;
         return $this;
     }
 
     /**
      * Retrieve the event manager
      *
+     * Lazy-loads an EventManager instance if none registered.
+     *
      * @return EventManagerInterface
      */
-    public function getEventManager() {
+    public function getEventManager()
+    {
+        if (!$this->events instanceof EventManagerInterface) {
+            $this->setEventManager(new EventManager());
+        }
         return $this->events;
     }
-
 }
