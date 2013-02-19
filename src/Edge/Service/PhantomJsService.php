3a17ae7c73c3cce54577e432f6cafc6fd3ec6e48
David Windell <david@outeredgeuk.com>
2013-02-19 18:25:18 +0000

    Added PhantomJS Service
    Signed-off-by: David Windell <david@outeredgeuk.com>

---
+++ src/Edge/Service/PhantomJsService.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Edge\Service;
+
+class PhantomJsService
+{
+    protected $bin = 'phantomjs';
+
+    public function __construct($bin = null)
+    {
+        if (null !== $bin) {
+            $this->bin = $bin;
+        }
+    }
+
+    public function runCommand()
+    {
+        $args = func_get_args();
+        $cmd = escapeshellcmd("{$this->bin} " . implode(' ', $args));
+        $result = exec($cmd, $output, $code);
+
+        if ($code > 0) {
+            throw new \Exception(sprintf('Unable to run command. %s', $result));
+        }
+
+        return $result;
+    }
+}
