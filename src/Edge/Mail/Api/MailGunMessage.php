c3f530bad551078ac629ce34733055557a4ffe38
David Windell <david@outeredgeuk.com>
2013-05-21 09:26:12 +0100

    Remove unused function

--- src/Edge/Mail/Api/MailGunMessage.php
+++ src/Edge/Mail/Api/MailGunMessage.php
@@ -64,15 +64,6 @@ class MailGunMessage extends Message
         return $this->html;
     }
 
-    public function getStrippedBody()
-    {
-        $body = $this->getStrippedBodyText();
-        if (empty($body)) {
-            $body = $this->getStrippedBodyHtml();
-        }
-        return $body;
-    }
-
     public function getStrippedBodyText()
     {
         return $this->getHeader('stripped-text');
