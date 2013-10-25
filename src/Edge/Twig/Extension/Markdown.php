d00572bb66c16c4d8fe8b75006191afe6ebc0816
David Windell <david@outeredgeuk.com>
2013-10-25 13:37:29 +0100

    Added Twig Markdown Filter

---
+++ src/Edge/Twig/Extension/Markdown.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Edge\Twig\Extension;
+
+use Edge\Markdown\GithubFlavouredMarkdown;
+use Edge\Markdown\MarkdownInterface;
+use Twig_Extension;
+use Twig_Filter_Method;
+
+class Markdown extends Twig_Extension
+{
+    /**
+     * @var MarkdownInterface
+     */
+    protected $markdown;
+
+    public function getName()
+    {
+        return 'Markdown';
+    }
+
+    public function getFilters()
+    {
+        return array(
+            'markdown' => new Twig_Filter_Method($this, 'transform', array('is_safe' => array('html'))),
+        );
+    }
+
+    /**
+     * Transfer text to html
+     *
+     * @param string $text
+     * @return string
+     */
+    public function transform($text)
+    {
+        return $this->getMarkdown()->transform($text);
+    }
+
+    /**
+     * Set the Markdown parser to use
+     *
+     * @param \Edge\Markdown\MarkdownInterface $markdown
+     */
+    public function setMarkdown(MarkdownInterface $markdown)
+    {
+        $this->markdown = $markdown;
+    }
+
+    /**
+     * @return MarkdownInterface
+     */
+    public function getMarkdown()
+    {
+        if (null === $this->markdown) {
+            $this->markdown = new GithubFlavouredMarkdown();
+        }
+
+        return $this->markdown;
+    }
+}
