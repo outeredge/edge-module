99dbdfb514ccf10f3e9d78ec8765255dd4951bc4
David Windell <david@outeredgeuk.com>
2014-05-08 16:01:39 +0100

    remove unused test methods

--- src/Edge/Test/AbstractRepositoryServiceTestCase.php
+++ src/Edge/Test/AbstractRepositoryServiceTestCase.php
@@ -3,7 +3,6 @@
 namespace Edge\Test;
 
 use Edge\Service\AbstractRepositoryService;
-use PHPUnit_Framework_ExpectationFailedException;
 
 abstract class AbstractRepositoryServiceTestCase extends AbstractTestCase
 {
@@ -30,44 +29,6 @@ abstract class AbstractRepositoryServiceTestCase extends AbstractTestCase
     protected function getService()
     {
         return $this->service;
-    }
-
-    /**
-     * Assert that an entity was returned from a create action
-     *
-     * @param string $entityClass
-     * @param mixed  $result
-     * @throws PHPUnit_Framework_ExpectationFailedException
-     */
-    public function assertEntityCreated($entityClass, $result)
-    {
-        if (!$result instanceof $entityClass) {
-            throw new PHPUnit_Framework_ExpectationFailedException(sprintf(
-                'Failed asserting that a "%s" entity was created: ' . "\n%s",
-                $entityClass,
-                print_r($this->getService()->getErrorMessages(), true)
-            ));
-        }
-        $this->assertInstanceOf($entityClass, $result);
-    }
-
-    /**
-     * Assert that an entity was returned from an update action
-     *
-     * @param string $entityClass
-     * @param mixed  $result
-     * @throws PHPUnit_Framework_ExpectationFailedException
-     */
-    public function assertEntityUpdated($entityClass, $result)
-    {
-        if (!$result instanceof $entityClass) {
-            throw new PHPUnit_Framework_ExpectationFailedException(sprintf(
-                'Failed asserting that a "%s" entity was updated: ' . "\n%s",
-                $entityClass,
-                print_r($this->getService()->getErrorMessages(), true)
-            ));
-        }
-        $this->assertInstanceOf($entityClass, $result);
     }
 
     public function tearDown()
