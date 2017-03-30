e466b9df533e324cdbd8d2f2f808cd72331137cb
David Windell <david@outeredgeuk.com>
2017-03-30 22:45:30 +0100

    Add pipeCommand() to phantomjs

--- src/Edge/Service/PhantomJsService.php
+++ src/Edge/Service/PhantomJsService.php
@@ -13,24 +13,84 @@ class PhantomJsService
         }
     }
 
+    /**
+     * Run PhantomJs and return the last command output
+     *
+     * @return string
+     * @throws Exception\RuntimeException
+     */
     public function runCommand()
     {
-        $args = array();
-        foreach (func_get_args() as $arg) {
-            $args[] = escapeshellarg($arg);
-        }
-
-        $cmd = escapeshellcmd("{$this->bin} --ignore-ssl-errors=true " . implode(' ', $args));
-
         $output = array();
         $code = 1;
 
-        $result = exec($cmd, $output, $code);
+        $result = exec($this->getCommand(func_get_args()), $output, $code);
 
         if ($code > 0) {
             throw new Exception\RuntimeException(sprintf('Unable to run command. %s', $result));
         }
 
-        return implode(PHP_EOL, $output);
+        return $result;
+    }
+
+    /**
+     * Pipe command to PhantomJS, expects first argument to be the content
+     *
+     * @return string resulting output
+     * @throws Exception\RuntimeException
+     */
+    public function pipeCommand()
+    {
+        $input = null;
+        $args  = [];
+        $pipes = [];
+
+        foreach (func_get_args() as $key => $arg) {
+            if ($key == 0) {
+                $input  = $arg;
+            } else {
+                $args[] = $arg;
+            }
+        }
+
+        $process = proc_open(
+            $this->getCommand($args),
+            [
+                0 => ['pipe', 'r'],
+                1 => ['pipe', 'w'],
+                2 => ['pipe', 'w']
+            ],
+            $pipes
+        );
+
+        if (!is_resource($process)) {
+            throw new Exception\RuntimeException('Unable to get resource');
+        }
+
+        fwrite($pipes[0], $input);
+        fclose($pipes[0]);
+
+        $result = stream_get_contents($pipes[1]);
+        $error  = stream_get_contents($pipes[2]);
+
+        fclose($pipes[1]);
+        fclose($pipes[2]);
+
+        $code = proc_close($process);
+
+        if ($code > 0 || !empty($error)) {
+            throw new Exception\RuntimeException(sprintf('Unable to run command. %s', $error));
+        }
+
+        return $result;
+    }
+
+    protected function getCommand(array $args)
+    {
+        foreach ($args as &$arg) {
+            $arg = escapeshellarg($arg);
+        }
+
+        return escapeshellcmd("{$this->bin} --ignore-ssl-errors=true " . implode(' ', $args));
     }
 }
